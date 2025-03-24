c6a6c5bc16d26e1ce9ba0f415d4a745c682c70e2
Brandon Mocco <bmocc92@yahoo.com>
2025-03-24 10:02:17 -0400

    Delete src directory

--- src/components/Recommender.tsx
+++
@@ -1,6 +0,0 @@
-// useEffect(() => {
-//     fetch(`/api/recommend/${seedTrackId}/`)
-//       .then(res => res.json())
-//       .then(data => setRecommendations(data));
-//   }, [seedTrackId]);
-  
